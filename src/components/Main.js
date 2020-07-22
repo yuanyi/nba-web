94f2156b57997fb25f10a75ab05bf9c96e757f36
yuanyi <yuanyi28@gmail.com>
2020-07-21 22:20:56 -0700

    TopBar and Profile Added

---
+++ src/components/Main.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+
+import Profile from './Profile'
+import nba from '../nba-client';
+import ShotChart from './ShotChart';
+
+console.log(nba);
+
+class Main extends Component {
+  state = {
+       playerInfo: {}
+   }
+
+   componentDidMount() {
+       window.nba = nba;
+       nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
+           .then((info) => {
+               console.log(info);
+               const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+               console.log(playInfo);
+               this.setState({ playerInfo: playInfo });
+           })
+   }
+
+
+   render() {
+       return (
+           <div className="main">
+              <Profile playerInfo={this.state.playerInfo}/>
+              <ShotChart />
+           </div>
+       );
+   }
+}
+
+export default Main;
---
+++ src/components/ShotChart.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ShotChart extends Component {
+   render() {
+       return (
+           <div>
+               shotchart
+           </div>
+       );
+   }
+}
+
+export default ShotChart;
