ad11662864f37ffa7fbe8d57c48f64c4878c9cde
yuanyi <yuanyi28@gmail.com>
2020-07-26 00:16:25 -0700

    add CountSlider

---
+++ src/components/CountSlider.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Slider, InputNumber, Row, Col } from 'antd';
+
+class CountSlider extends React.Component {
+ state = {
+    inputValue: this.props.value
+ }
+
+ onChange = value => {
+    this.setState({
+      inputValue: value,
+    });
+    this.props.onCountSliderChange(value);
+  };
+
+ render() {
+   const { inputValue } = this.state;
+    return (
+      <Row>
+        <Col span={12}>
+          <Slider
+            min={1}
+            max={20}
+            onChange={this.onChange}
+            value={ inputValue }
+          />
+        </Col>
+        <Col span={4}>
+          <InputNumber
+            min={1}
+            max={20}
+            style={{ margin: '0 16px' }}
+            value={inputValue}
+            onChange={this.onChange}
+          />
+        </Col>
+      </Row>
+    );
+ }
+}
+
+export default CountSlider;
--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,12 +1,26 @@
 import React, {Component} from 'react';
-
+import CountSlider from './CountSlider'
 import ShotChart from './ShotChart';
 
 class DataViewContainer extends Component {
+  state = {
+      minCount: 2
+  }
+
+  onCountSliderChange = (data) => {
+    console.log(data);
+  }
+
    render() {
        return (
            <div className="data-view">
-               <ShotChart playerId={this.props.playerId}/>
+               <ShotChart playerId={this.props.playerId}
+                          minCount={this.state.minCount}
+               />
+
+               <div className="filters">
+                  <CountSlider onCountSliderChange={this.onCountSliderChange}/>
+               </div>
            </div>
        );
    }
