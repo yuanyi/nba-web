249c2103e608c27181c7293f16b97a5b515fe26f
Lifu Yao <lifuyao@hnb-share-router.xiaojukeji.com>
2020-08-05 01:17:53 -0700

    update

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,4 @@
 import React, { Component, useState } from 'react';
-
 import { Input, AutoComplete } from 'antd';
 import { SelectProps } from 'antd/es/select';
 // import { Icon, Button, Input, AutoComplete } from 'antd';
@@ -7,38 +6,66 @@ import { SelectProps } from 'antd/es/select';
 
 class SearchBar extends Component {
 
-  const Complete: React.FC = () => {
+function getRandomInt(max, min = 0) {
+  return Math.floor(Math.random() * (max - min + 1)) + min; // eslint-disable-line no-mixed-operators
+}
 
-    const [options, setOptions] = useState<SelectProps<object>['options']>([]);
-
-      const handleSearch = (value: string) => {
-        setOptions(value ? searchResult(value) : []);
-      };
-
-      const onSelect = value => {
-        console.log('onSelect', value);
-      };
-
-
-      render() {
-        return (
-          <div>
-            <AutoComplete
-              dropdownMatchSelectWidth={252}
-              style={{
-                width: 300,
-              }}
-              options={options}
-              onSelect={onSelect}
-              onSearch={handleSearch}
-            >
-              <Input.Search size="large" placeholder="input here" enterButton />
-            </AutoComplete>
+const searchResult = query =>
+  new Array(getRandomInt(5))
+    .join('.')
+    .split('.')
+    .map((item, idx) => {
+      const category = `${query}${idx}`;
+      return {
+        value: category,
+        label: (
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <span>
+              Found {query} on{' '}
+              <a
+                href={`https://s.taobao.com/search?q=${query}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {category}
+              </a>
+            </span>
+            <span>{getRandomInt(200, 100)} results</span>
           </div>
-        );
-      }
-  }
+        ),
+      };
+    });
+render() {
 
+  const [options, setOptions] = useState([]);
+
+  const handleSearch = value => {
+    setOptions(value ? searchResult(value) : []);
+  };
+
+  const onSelect = value => {
+    console.log('onSelect', value);
+  };
+
+  return (
+    <AutoComplete
+      dropdownMatchSelectWidth={252}
+      style={{
+        width: 300,
+      }}
+      options={options}
+      onSelect={onSelect}
+      onSearch={handleSearch}
+    >
+      <Input.Search size="large" placeholder="input here" enterButton />
+    </AutoComplete>
+  );
+}
 
 }
 
